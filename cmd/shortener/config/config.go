32f470d787ed055544c749e196c9f3a2d2b41291
Daniil Sud <d.sud@crpt.ru>
2023-11-04 02:48:46 +0300

    regress iter4 fix

--- cmd/shortener/config/config.go
+++ cmd/shortener/config/config.go
@@ -9,14 +9,9 @@ import (
 
 // var errInvalidFormat error = errors.New("address must be in format 'host:port'. i.e. localhost:8080")
 
-const (
-	defaultServerAddress   = ":8080"
-	defaultResponseAddress = "http://localhost:8080"
-)
-
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	ResponseAddress string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	ServerAddress   string `env:"SERVER_ADDRESS"`
+	ResponseAddress string `env:"BASE_URL"`
 }
 
 func (cfg *Config) ParseEnv() {
@@ -24,8 +19,6 @@ func (cfg *Config) ParseEnv() {
 }
 
 func (cfg *Config) ParseFlags() {
-	cfg.ServerAddress = defaultServerAddress
-	cfg.ResponseAddress = defaultResponseAddress
 	flag.Func("a", "Example: -a localhost:8080", func(v string) error {
 		if err := validateAddress(v); err != nil {
 			return err
